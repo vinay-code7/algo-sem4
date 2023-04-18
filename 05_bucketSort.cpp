972f6fa019621f8683c27443e9464e05511ab1c2
vinaycode7@gmail.com <vinaycode7@gmail.com>
2023-04-18 09:13:38 +0530

    bucketSort

--- 05_bucketSort.cpp
+++ 05_bucketSort.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 void input(int arr[], int n)
@@ -17,7 +19,21 @@ void print(int arr[], int n)
 
 void bucketSort(float arr[], int n)
 {
+    vector<int> brr[10];
     
+    for (int i = 0; i < n; i++)
+    {
+        int idx = arr[i] / 10;
+        brr[idx].push_back(arr[i]);
+    }
+
+    for (int i = 0; i < 10; i++)
+        sort(brr[i].begin(), brr[i].end());
+
+    int idx = 0;
+    for (int i = 0; i < 10; i++)
+        for (int j = 0; j < brr[i].size(); j++)
+            arr[idx++] = brr[i][j];
 }
 
 int main()
@@ -40,3 +56,4 @@ int main()
         cout << arr[i] << " ";
     cout << endl;
 }
+// 78 17 39 26 72 94 21 12 23 68
